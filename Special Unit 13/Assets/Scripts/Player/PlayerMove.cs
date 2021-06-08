84e86e95505b9ffd002198b7e52e9052228173d4
kodlon <kodlon.igorchuchman@gmail.com>
2021-06-08 16:37:01 +0300

    Create Project and test script

---
+++ Special Unit 13/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private void Start() {
+        Debug.Log("Test");
+    }
+}
